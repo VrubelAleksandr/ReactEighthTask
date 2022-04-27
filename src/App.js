02d1a90cf47af8f8f1e108bed5476fdff54bb6a7
VrubelAleksandr <vrubelaleksandr@gmail.com>
2022-04-27 16:24:12 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MyForm from "./form/MyForm";
+import "./App.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <MyForm />
+    </div>
+  );
+}
